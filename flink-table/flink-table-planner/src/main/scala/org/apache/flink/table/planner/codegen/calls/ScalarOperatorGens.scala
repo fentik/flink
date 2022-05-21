d053df3952daff767cea30b9b65ad9959f9e6826
Sergei Tsarev <sergei@fentik.com>
2022-05-21 15:30:50 +0000

    potential fix for issue #107 : bug in greatest()
    The order of variable initialization, assigment, and access is
    incorrect. The existing code allowed for what were effectively
    unitialized variable reads.
    
    Fix declaration of local state variables, evaluation order
    expression terms, and assignment + evaliation of terms.

--- flink-table/flink-table-planner/src/main/scala/org/apache/flink/table/planner/codegen/calls/ScalarOperatorGens.scala
+++ flink-table/flink-table-planner/src/main/scala/org/apache/flink/table/planner/codegen/calls/ScalarOperatorGens.scala
@@ -1398,6 +1398,10 @@ object ScalarOperatorGens {
     val elementsCode = elements.map { element =>
       s"""
          | ${element.code}
+         | // boxedResultTypeTerm tmpResult = {castIfNumeric(elements.head)}; 
+         | $boxedResultTypeTerm $tmpResult = ${castIfNumeric(elements.head)};
+         | // primitiveResultTypeTerm result = {primitiveDefaultValue(widerType.get)};
+         | $primitiveResultTypeTerm $result = ${primitiveDefaultValue(widerType.get)};
          | if (!$nullTerm) {
          |   $boxedResultTypeTerm $cur = ${castIfNumeric(element)};
          |   if (${element.nullTerm}) {
@@ -1414,10 +1418,11 @@ object ScalarOperatorGens {
 
     val code =
       s"""
-         | $boxedResultTypeTerm $tmpResult = ${castIfNumeric(elements.head)};
-         | $primitiveResultTypeTerm $result = ${primitiveDefaultValue(widerType.get)};
+         | // boolean nullTerm = false;
          | boolean $nullTerm = false;
+         | // elementsCode
          | $elementsCode
+         | // if (!nullTerm) {
          | if (!$nullTerm) {
          |   $result = $tmpResult;
          | }
