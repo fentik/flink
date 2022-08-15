f4d9b54a7a5242073c9ff0695ec1a2a15d8fb529
Sergei Tsarev <sergei@fentik.com>
2022-08-15 02:39:57 +0000

    remove unused code

--- flink-table/flink-table-runtime/src/main/java/org/apache/flink/table/runtime/operators/sink/SinkUpsertMaterializer.java
+++ flink-table/flink-table-runtime/src/main/java/org/apache/flink/table/runtime/operators/sink/SinkUpsertMaterializer.java
@@ -124,60 +124,6 @@ public class SinkUpsertMaterializer extends TableStreamOperator<RowData>
         this.collector = new TimestampedCollector<>(output);
     }
 
-    // TODO(akhilg): This is an exact copy of a function in AbstractStreamingOperator. When you are getting
-    // bored, figure out which class/interface can be put this function in and re-use between these two classes.
-    protected static String rowToString(RowType type, RowData row) {
-        LogicalType[] fieldTypes =
-                type.getFields().stream()
-                        .map(RowType.RowField::getType)
-                        .toArray(LogicalType[]::new);
-        String[] fieldNames = type.getFieldNames().toArray(new String[0]);
-        int rowArity = type.getFieldCount();
-        String rowString = "";
-        for (int i = 0; i < rowArity; i++) {
-            String value = "";
-            if (row.isNullAt(i)) {
-                value = "<NULL>";
-            } else {
-                switch (fieldTypes[i].getTypeRoot()) {
-                        case NULL:
-                            value = "<NULL>";
-                            break;
-
-                        case BOOLEAN:
-                            value = row.getBoolean(i) ? "True" : "False";
-                            break;
-
-                        case INTEGER:
-                        case INTERVAL_YEAR_MONTH:
-                            value = Integer.toString(row.getInt(i));
-                            break;
-
-                        case BIGINT:
-                        case INTERVAL_DAY_TIME:
-                            value = Long.toString(row.getLong(i));
-                            break;
-
-                        case CHAR:
-                        case VARCHAR:
-                            value = row.getString(i).toString();
-                            break;
-
-                        case DATE:
-                            value = "[DATE TYPE]";
-                            break;
-
-                        default:
-                            value = "[Unprocessed type]";
-                            break;
-                }
-            }
-            String field = fieldNames[i] + "=" + value;
-            rowString = rowString + field + (i == rowArity - 1 ? "" : ", ");
-        }
-        return rowString;
-    }
-
     @Override
     public void processElement(StreamRecord<RowData> element) throws Exception {
         final RowData row = element.getValue();
