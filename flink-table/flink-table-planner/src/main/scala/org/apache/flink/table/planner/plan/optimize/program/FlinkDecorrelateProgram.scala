e5d0928f73da139d0759bac481e9e7d57ec07b36
Akhil Gupta <akhilg@fentik.com>
2022-09-20 11:42:43 -0700

    Revert "Comment out the exception to see if this fixes the issue"
    This reverts commit 78ae016fc7b34eebb94bfc401e96930106a3465f.

--- flink-table/flink-table-planner/src/main/scala/org/apache/flink/table/planner/plan/optimize/program/FlinkDecorrelateProgram.scala
+++ flink-table/flink-table-planner/src/main/scala/org/apache/flink/table/planner/plan/optimize/program/FlinkDecorrelateProgram.scala
@@ -26,7 +26,6 @@ import org.apache.calcite.rel.{RelNode, RelShuttleImpl}
 import org.apache.calcite.rex.{RexCorrelVariable, RexVisitorImpl}
 import org.apache.calcite.sql2rel.RelDecorrelator
 import org.apache.calcite.util.Util
-import org.apache.flink.table.planner.utils.Logging
 
 import scala.collection.JavaConversions._
 
@@ -36,7 +35,7 @@ import scala.collection.JavaConversions._
   *
   * @tparam OC OptimizeContext
   */
-class FlinkDecorrelateProgram[OC <: FlinkOptimizeContext] extends FlinkOptimizeProgram[OC] with Logging {
+class FlinkDecorrelateProgram[OC <: FlinkOptimizeContext] extends FlinkOptimizeProgram[OC] {
 
   def optimize(root: RelNode, context: OC): RelNode = {
     val result = RelDecorrelator.decorrelateQuery(root)
@@ -54,15 +53,9 @@ class FlinkDecorrelateProgram[OC <: FlinkOptimizeContext] extends FlinkOptimizeP
     try {
       checkCorrelVariableOf(root)
     } catch {
-       case fo: Util.FoundOne => {
-        LOG.warn("Found an correlated value which we are ignoring")
-       }
-  /**  TODO(akhilg): Commenting it out since it gets triggered when we have a CROSS join
-    *  with nested queries. See https://github.com/fentik/dataflo/issues/432
-    */
-  /***     throw new TableException(s"unexpected correlate variable " +
-  *        s"${fo.getNode.asInstanceOf[RexCorrelVariable].id} in the plan")
-  */
+      case fo: Util.FoundOne =>
+        throw new TableException(s"unexpected correlate variable " +
+          s"${fo.getNode.asInstanceOf[RexCorrelVariable].id} in the plan")
     }
   }
 
