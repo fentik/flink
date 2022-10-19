471a21311d68312055b56cbe9fe93151adfae8d0
Sergei Tsarev <sergeitsar@users.noreply.github.com>
2022-10-18 21:20:45 -0700

    Release 1.15.0 rc1 fentik cdc calc pushdown planner (#17)
    push projections past ChangelogNormalize
    - limits normalizer state
    - suppresses updates for unused columns

--- flink-table/flink-table-planner/src/main/scala/org/apache/flink/table/planner/delegation/PlannerBase.scala
+++ flink-table/flink-table-planner/src/main/scala/org/apache/flink/table/planner/delegation/PlannerBase.scala
@@ -63,6 +63,7 @@ import org.apache.flink.table.planner.utils.JavaScalaConversionUtil.{toJava, toS
 import org.apache.flink.table.runtime.generated.CompileUtils
 import org.apache.flink.table.sinks.TableSink
 import org.apache.flink.table.types.utils.LegacyTypeInfoDataTypeConverter
+import org.apache.flink.table.planner.plan.rules.physical.stream.PushCalcsPastChangelogNormalize
 
 import java.lang.{Long => JLong}
 import java.util
@@ -302,14 +303,16 @@ abstract class PlannerBase(
   private[flink] def optimize(relNodes: Seq[RelNode]): Seq[RelNode] = {
     val optimizedRelNodes = getOptimizer.optimize(relNodes)
     require(optimizedRelNodes.size == relNodes.size)
-    optimizedRelNodes
+    // XXX(sst): in 1.16, there's a hook for this use case, but for now hack it
+    PushCalcsPastChangelogNormalize.optimize(getRelBuilder, optimizedRelNodes)
   }
 
   @VisibleForTesting
   private[flink] def optimize(relNode: RelNode): RelNode = {
     val optimizedRelNodes = getOptimizer.optimize(Seq(relNode))
     require(optimizedRelNodes.size == 1)
-    optimizedRelNodes.head
+    // XXX(sst): in 1.16, there's a hook for this use case, but for now hack it
+    PushCalcsPastChangelogNormalize.optimize(getRelBuilder, optimizedRelNodes).head
   }
 
   /**
